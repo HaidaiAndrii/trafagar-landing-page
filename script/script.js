b357b4359239d539af30e7d84be0dcc342f37c0d
andriihaidai <haidai.andriy@gmail.com>
2021-04-27 15:16:13 +0300

    fposition was added

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
+const modal = `
+<form onsubmit="return false" class="search-item-form modal__search">
+        <input type='text' class="search-input" /><br/>
+        <span class='search-span-error'>Elemet not found (404)  (. )( .)</span><br/>
+        <button name="search" class="search-button">search</button>
+        <button name="Clear" class="clear-search-select" disabed>Clear</button>
+        <div class="search-form-elemnet-change">
+        
+        <button name="next" class="select-next-element" disabled>Next</button>
+        <button name="prev" class="select-prec-element" disabled>Prev</button>
+        <button name="child" class="select-child-element" disabled>Child</button>
+            <button name="parent" class="select-parent-element" disabled>Parent</button>
+            </div>
+            </form>`;
+const main = document.querySelector('body');
+main.insertAdjacentHTML('beforebegin', modal);
+
+const searchModal = document.querySelector('.modal__search');
+
 let searchButton = document.querySelector('.search-button');
 let nextElButton = document.querySelector('.select-next-element');
 let searchInput = document.querySelector('.search-input');
@@ -6,6 +25,20 @@ let childElButton = document.querySelector('.select-child-element');
 let parentElButton = document.querySelector('.select-parent-element');
 let clearInputButton = document.querySelector('.clear-search-select');
 let searchformButtonSection = document.querySelector('.search-form-elemnet-change');
+let searchSpanError = document.querySelector('.search-span-error');
+
+searchSpanError.style.color = 'white';
+searchSpanError.style.visibility = 'hidden';
+
+searchModal.style.padding = '50px';
+searchModal.style.width = `300 px `;
+searchModal.style.height = `300 px `;
+searchModal.style.backgroundColor = `black `;
+
+function scrollToElemnt(element) {
+    window.scrollTo(pageXOffset, element.offsetTop);
+};
+
 
 let element;
 let border = '';
@@ -32,7 +65,13 @@ searchButton.addEventListener('click', () => {
     }
     if (searchInput.value) {
         element = document.querySelector(`${searchInput.value}`);
-        console.log(element.nextElementSibling);
+        if (!element) {
+            searchSpanError.style.visibility = '';
+
+        } else {
+            scrollToElemnt(element);
+            searchSpanError.style.visibility = 'hidden';
+        }
     }
     if (element) {
         checkElements();
@@ -44,8 +83,9 @@ clearInputButton.addEventListener('click', () => {
     if (element) {
         unSelectElement();
         element = '';
+        checkElements();
     }
-    checkElements();
+    searchSpanError.style.visibility = 'hidden';
     searchInput.value = '';
 })
 
@@ -74,36 +114,35 @@ searchformButtonSection.addEventListener('click', function(event) {
     checkElements();
     return undefined;
 })
-const searchModal = document.querySelector('.modal__search');
+searchModal.style.position = 'fixed';
 
-searchModal.style.padding = '50px';
-searchModal.style.position = 'absolute';
-searchModal.style.top = '100px';
-searchModal.style.width = `300 px `;
-searchModal.style.height = `300 px `;
-searchModal.style.backgroundColor = `black `;
+let x = 0;
+let y = 0;
 
-searchModal.onmousedown = function(event) {
-    let shiftX = event.clientX - searchModal.getBoundingClientRect().left;
-    let shiftY = event.clientY - searchModal.getBoundingClientRect().top;
-    searchModal.style.zIndex = 10;
 
-    function moveAt(pageX, pageY) {
-        searchModal.style.left = pageX - shiftX + 'px';
-        searchModal.style.top = pageY - shiftY + 'px';
-    }
+const mouseDownHandler = function(e) {
+    x = e.clientX;
+    y = e.clientY;
 
-    moveAt(event.pageX, event.pageY);
-
-    function onMouseMove(e) {
-        moveAt(e.pageX, e.pageY);
-    }
-    document.addEventListener('mousemove', onMouseMove);
-    searchModal.onmouseup = function() {
-        document.removeEventListener('mousemove', onMouseMove);
-        searchModal.onmouseup = null;
-    };
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
 };
-searchModal.ondragstart = function() {
-    return false;
+
+const mouseMoveHandler = function(e) {
+    searchModal.style.zIndex = 100;
+    const dx = e.clientX - x;
+    const dy = e.clientY - y;
+
+    searchModal.style.top = `${searchModal.offsetTop + dy}px`;
+    searchModal.style.left = `${searchModal.offsetLeft + dx}px`;
+
+    x = e.clientX;
+    y = e.clientY;
 };
+
+const mouseUpHandler = function() {
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+};
+
+searchModal.addEventListener('mousedown', mouseDownHandler);
